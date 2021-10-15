72fbad51e782d7010e976b9095376b6638050699
Lars Radtke <lars.radtke@gmx.net>
2021-10-16 01:51:10 +0200

    uppercase Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/macro';
 export const Header = () => {
   return (
     <LogoWrapper>
-      <Logo>pixxelschubser</Logo>
+      <Logo>PIXXELSCHUBSER</Logo>
     </LogoWrapper>
   );
 };
