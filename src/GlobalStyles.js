a45563af1b2b5e1f4b1203b381ec027578eefda5
Lars Radtke <lars.radtke@gmx.net>
2021-10-21 09:41:14 +0200

    upddate Global Styles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -2,12 +2,20 @@ import { createGlobalStyle } from 'styled-components';
 import './Montserrat.css';
 
 export default createGlobalStyle`
+	:root {
+		--col-white: rgba(220,220,220,1);
+		--col-white-trans: rgba(220,220,220,0.3);
+		--col-grey: rgba(76,76,76,1);
+		--col-black: rgba(15,15,15,1);
+		--col-black-trans: rgba(0,0,0,0.7);
+	}
+
 	* {
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
 		font-family: "Montserrat", sans-serif;
-		color: rgba(220,220,220,1);
+		color: var(--col-white);
 		line-height: 1.5;
 		scroll-behavior: smooth;
 		cursor: default;
@@ -15,6 +23,6 @@ export default createGlobalStyle`
 
 	body {
 		height: 100vh;
-		background-color: rgba(15,15,15,1);
+		background-color: #000;
 	}
 `;
