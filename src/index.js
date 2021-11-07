eba1ca14c346144ab4e83db2c732fe44fe57971d
Lars Radtke <lars.radtke@gmx.net>
2021-11-07 19:57:56 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from 'components';
+import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
 
 import reportWebVitals from './reportWebVitals';
